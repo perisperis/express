d5344fe77692f81251064931cce2557a4cf3d3f9
Garcia <A508571@deac.awl.atosorigin.net>
2020-04-25 18:50:44 +0200

    Updates

---
+++ precache-manifest.dd143b0e7c632ebebb2004bd41d3aaf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffae3bfc310bac027772172519c79ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6a31dd9ea1a8aae5540",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c2ffd01b0a90bf7a556c",
+    "url": "/static/js/2.78886ae4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78886ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a31dd9ea1a8aae5540",
+    "url": "/static/js/main.0949f9a5.chunk.js"
+  },
+  {
+    "revision": "380be84c68db49142664",
+    "url": "/static/js/runtime-main.daa00a9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
